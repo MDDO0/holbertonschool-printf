a1c14709454049ebfdfdf0801bfa3ad43cce8729
“MDDO0” <maado321@gmail.com>
2025-04-07 08:23:23 +0000

    a

--- _printf.c
+++ _printf.c
@@ -1,61 +1,47 @@
-#include <stdarg.h>
-#include <stddef.h>
 #include "main.h"
 
 /**
- * _printf - prints formatted string with c, s, %, d, and i
+ * _printf - custom printf function
  * @format: format string
  *
  * Return: number of characters printed
  */
 int _printf(const char *format, ...)
 {
+	int i = 0, count = 0;
 	va_list args;
-	int i, count = 0;
-	char *str;
 
 	if (format == NULL)
 		return (-1);
 
 	va_start(args, format);
 
-	for (i = 0; format[i]; i++)
+	while (format[i])
 	{
-		if (format[i] == '%')
+		if (format[i] == '%' && format[i + 1])
 		{
 			i++;
-			if (format[i] == '\0')
-				return (-1);
-
 			if (format[i] == 'c')
-				count += _putchar(va_arg(args, int));
-
+				count += print_char(args);
 			else if (format[i] == 's')
-			{
-				str = va_arg(args, char *);
-				if (str == NULL)
-					str = "(null)";
-				while (*str)
-					count += _putchar(*str++);
-			}
-
+				count += print_string(args);
 			else if (format[i] == '%')
-				count += _putchar('%');
-
-			else if (format[i] == 'd' || format[i] == 'i')
-				count += print_number(va_arg(args, int));
-
+				count += print_percent();
 			else
 			{
-				count += _putchar('%');
-				count += _putchar(format[i]);
+				_putchar('%');
+				_putchar(format[i]);
+				count += 2;
 			}
 		}
 		else
-			count += _putchar(format[i]);
+		{
+			_putchar(format[i]);
+			count++;
+		}
+		i++;
 	}
 
 	va_end(args);
-
 	return (count);
 }
