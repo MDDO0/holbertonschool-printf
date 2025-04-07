5d2c6528d2b49620c87c408b8590159211569403
“MDDO0” <maado321@gmail.com>
2025-04-07 08:23:07 +0000

    a

--- main.h
+++ main.h
@@ -1,7 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdarg.h>
+#include <unistd.h>
+
 int _printf(const char *format, ...);
+int print_char(va_list args);
+int print_string(va_list args);
+int print_percent(void);
 int _putchar(char c);
-int print_number(int n);
+
 #endif
