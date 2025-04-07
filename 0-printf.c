f3556ef1f209ba062d05eb1f755ad0bb016029d7
“MDDO0” <maado321@gmail.com>
2025-04-07 09:24:02 +0000

    a

--- 0-printf.c
+++ 0-printf.c
@@ -1,82 +1,45 @@
 #include "main.h"
-/**
- * print_char - prints a single character
- * @args: va_list containing the character
- * Return: number of characters printed
- */
-int print_char(va_list args)
-{
-	return (_putchar(va_arg(args, int)));
-}
 
 /**
- * print_string - prints a string
- * @args: va_list containing the string
- * Return: number of characters printed
- */
-int print_string(va_list args)
-{
-	char *s = va_arg(args, char *);
-	int i = 0;
-
-	if (s == NULL)
-		s = "(null)";
-	while (s[i])
-	{
-		_putchar(s[i]);
-		i++;
-	}
-	return (i);
-}
-
-/**
- * print_percent - prints a percent sign
- * Return: 1
- */
-int print_percent(void)
-{
-	return (_putchar('%'));
-}
-
-/**
- * _printf - custom printf function
+ * _printf - produces output according to a format
  * @format: format string
+ *
  * Return: number of characters printed
  */
 int _printf(const char *format, ...)
 {
 	va_list args;
-	int i = 0, count = 0;
+	int i = 0, printed = 0;
 
-	if (!format)
+	if (format == NULL)
 		return (-1);
+
 	va_start(args, format);
 
-	while (format && format[i])
+	while (format[i])
 	{
 		if (format[i] == '%' && format[i + 1])
 		{
 			i++;
 			if (format[i] == 'c')
-				count += print_char(args);
+				printed += print_char(args);
 			else if (format[i] == 's')
-				count += print_string(args);
+				printed += print_string(args);
 			else if (format[i] == '%')
-				count += print_percent();
+				printed += print_percent();
 			else
 			{
-				_putchar('%');
-				_putchar(format[i]);
-				count += 2;
+				printed += _putchar('%');
+				printed += _putchar(format[i]);
 			}
 		}
 		else
 		{
-			_putchar(format[i]);
-			count++;
+			printed += _putchar(format[i]);
 		}
 		i++;
 	}
+
 	va_end(args);
-	return (count);
+	return (printed);
 }
