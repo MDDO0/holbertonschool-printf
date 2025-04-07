e488dcfb8c21d7e7ac831bdeb1ff3f41fb9c7a44
“MDDO0” <maado321@gmail.com>
2025-04-07 09:23:19 +0000

    a

--- print_char.c
+++ print_char.c
@@ -1,7 +1,14 @@
 #include "main.h"
 
+/**
+ * print_char - prints a character
+ * @args: argument list
+ *
+ * Return: number of characters printed
+ */
 int print_char(va_list args)
 {
 	char c = va_arg(args, int);
+
 	return (_putchar(c));
 }
